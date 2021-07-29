d1bdf4ee7490d15fcee523779b921c2c8a5c40e8
big4oot <37404922+big4oot@users.noreply.github.com>
2021-07-29 14:06:24 +0300

    Delete bot_config.py

--- bot_config.py
+++
@@ -1,6 +0,0 @@
-bot_token = "1884196444:AAFrUqZhwBJUsX7AoBBl_x3nIHDOvVUnBTM"
-headers = {
-    'content-type': "application/json",
-    'x-apikey': "df4b77e21928f587b1a7802c45ae0331a2bb9",
-    'cache-control': "no-cache"
-    }
